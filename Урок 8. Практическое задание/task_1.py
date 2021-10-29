902f51c45a994d2701ffed75f9d8a1e73ce93d88
Vasily <berkach@gmail.com>
2021-10-29 19:07:59 +0300

    pr lesson8

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -9,3 +9,52 @@
 2) тема понятна? постарайтесь сделать свою реализацию.
 Вы можете реализовать задачу, например, через ООП или предложить иной подход к решению.
 """
+import collections
+
+
+class HaffMan:
+
+    def __init__(self, string_str):
+        self.string = string_str
+        self.code = dict()
+
+    def _prepare_string(self):
+        digits_frequency = collections.Counter(self.string)
+        sorted_elements = sorted(digits_frequency.items(), key=lambda val: val[1])
+        sorted_elements = collections.deque(sorted_elements)
+        return sorted_elements
+
+    def _build_tree(self):
+        cod_string = self._prepare_string()
+        while len(cod_string) > 1:
+            wight = cod_string[0][1] + cod_string[1][1]
+            temp = {
+                0: cod_string.popleft()[0],
+                1: cod_string.popleft()[0]
+            }
+            # print(temp, end=" - ")
+            # print(wight)
+            for index, value in enumerate(cod_string):
+                if wight <= value[1]:
+                    cod_string.insert(index, (temp, wight))
+                    break
+            else:
+                cod_string.append((temp, wight))
+                # print(index, end=" - ")
+                # print(value)
+            # print(cod_string)
+        return cod_string[0][0]
+
+    def codding(self, path="", tree=None):
+        if tree is None:
+            tree = self._build_tree()
+        if not isinstance(tree, dict):
+            self.code[tree] = path
+        else:
+            self.codding(f'{path}0', tree[0])
+            self.codding(f'{path}1', tree[1])
+        return self.code
+
+
+a = HaffMan("beep boop beer!")
+print(a.codding())
--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -10,6 +10,7 @@
 Поработайте с доработанной структурой, позапускайте на реальных данных - на клиентском коде.
 """
 
+
 class BinaryTree:
     def __init__(self, root_obj):
         # корень
@@ -20,9 +21,9 @@ class BinaryTree:
         self.right_child = None
 
     # добавить левого потомка
-    def insert_left(self, new_node):
+    def _insert_left(self, new_node):
         # если у узла нет левого потомка
-        if self.left_child == None:
+        if self.left_child is None:
             # тогда узел просто вставляется в дерево
             # формируется новое поддерево
             self.left_child = BinaryTree(new_node)
@@ -35,9 +36,9 @@ class BinaryTree:
             self.left_child = tree_obj
 
     # добавить правого потомка
-    def insert_right(self, new_node):
+    def _insert_right(self, new_node):
         # если у узла нет правого потомка
-        if self.right_child == None:
+        if self.right_child is None:
             # тогда узел просто вставляется в дерево
             # формируется новое поддерево
             self.right_child = BinaryTree(new_node)
@@ -48,6 +49,12 @@ class BinaryTree:
             # и спускаем имеющегося потомка на один уровень ниже
             tree_obj.right_child = self.right_child
             self.right_child = tree_obj
+
+    def insert_el(self, new_node):
+        if new_node < self.root:
+            self._insert_left(new_node)
+        else:
+            self._insert_right(new_node)
 
     # метод доступа к правому потомку
     def get_right_child(self):
@@ -66,14 +73,18 @@ class BinaryTree:
         return self.root
 
 
-r = BinaryTree(8)
-print(r.get_root_val())
-print(r.get_left_child())
-r.insert_left(40)
-print(r.get_left_child())
-print(r.get_left_child().get_root_val())
-r.insert_right(12)
-print(r.get_right_child())
-print(r.get_right_child().get_root_val())
-r.get_right_child().set_root_val(16)
-print(r.get_right_child().get_root_val())
+if __name__ == "__main__":
+    big_root = int(input("Введите число которое будет корнем: "))
+    r = BinaryTree(big_root)
+    print(r.get_root_val())
+    # print(r.get_left_child())
+    r.insert_el(40)
+    print(r.get_right_child().get_root_val())
+    r.insert_el(6)
+    r.get_left_child().insert_el(7)
+    print(r.get_left_child().get_root_val())
+    r.insert_el(5)
+    print(r.get_left_child().get_root_val())
+    r.get_right_child().set_root_val(16)
+    print(r.get_right_child().get_root_val())
+    # print(r.get_root_val())
